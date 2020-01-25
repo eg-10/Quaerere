8abd765ebffd8d7d6fad28fd66cd5432525e8fde
Ebrahim Gabajiwala <ebrahimgabajiwala@gmail.com>
2020-01-26 00:57:44 +0530

    Questions fetched and displayed on frontend

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light my-navbar">
+        <div class="container">
+            <router-link class="navbar-brand" :to="{ name: 'home' }">Quarere</router-link>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <router-link class="nav-link my-nav-link" :to="{ name: 'home' }">Home</router-link>
+                    </li>
+                    <li class="nav-item active">
+                        <router-link class="nav-link my-nav-link" :to="{ name: 'about' }">About</router-link>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link my-nav-link" href="/accounts/logout/">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavbarComponent"
+};
+</script>
+
+<style scoped>
+.my-navbar {
+    border-bottom: 1px solid #ddd;
+}
+
+.navbar-brand {
+    color: #ba2929;
+}
+
+.navbar-brand:hover {
+    color: #ff4747 !important;
+}
+
+.my-nav-link:hover {
+    color: #ba2929 !important;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App, peopoooooooooooooooooool"/>
-  </div>
+    <div class="home m-4">
+        <div class="container">
+            <div v-for="question in questions"
+                :key="question.pk" class="question">
+                <h2>{{ question.content }}</h2>
+                <p>Posted by:
+                    <span>{{ question.author }}</span>
+                </p>
+                <p>{{ question.answers_count }} Answer<span v-if="question.answers_count > 1">s</span></p>
+                <hr>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { apiService } from "../common/api.service.js"
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "home",
+    data() {
+        return {
+            questions: []
+        }
+    },
+    methods: {
+        getQuestions() {
+            let endpoint = "api/questions/";
+            apiService(endpoint)
+                .then(data => {
+                    this.questions.push(...data.results);
+                })
+        }
+    },
+    created() {
+        this.getQuestions()
+    }
+};
 </script>
+
+<style>
+.question {
+    text-align: left;
+}
+</style>
